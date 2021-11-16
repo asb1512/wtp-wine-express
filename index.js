e902ab8f19cf2f3bd7904715fda0a247fccd2dbb
asb1512 <andrew@olympicmedia.com>
2021-11-16 16:38:36 -0500

    configured app to use CORS and add additional error handling to the Stripe portion of the API

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
 const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
 const app = express();
+
+app.use(cors());
 
 app.get('/secret', async (req, res) => {
   if (req.query.amount) {
     const amount = req.query.amount
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency: 'usd',
-      payment_method_types: ['card'],
-    });
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+      });
+    } catch (error) {
+      res.status(400).json({ error: { message: error.message } })
+    }
 
-    res.json({client_secret: paymentIntent.client_secret})
+    res.json({
+      payment_intent_id: paymentIntent.id,
+      client_secret: paymentIntent.client_secret
+    })
 
   } else {
     res.status(400).send({
